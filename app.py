1389adf5b1f80151584b8328f8cb4064b9e8776a
Vishnu Rathore <vishnu.eps91@gmail.com>
2025-01-23 14:56:25 +0530

    added styling

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def hello_world():
+    return "Hello, World! 100ms 100ms"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
