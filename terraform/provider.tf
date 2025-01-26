cfe29300bc290beb53e7604568db49f4067b2408
Vishnu Rathore <81683825+Rkvishnu@users.noreply.github.com>
2025-01-26 20:25:28 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 locals {
   region         = "ap-south-1"
   name           = "flask-app-cluster"
